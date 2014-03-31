561878188bacce19a55cd0717c989897d7523231
Jelle Kralt <jelle@jellekralt.nl>
2014-03-31 21:41:50 +0200

    Version number +1

--- js/jquery.responsiveTabs.js
+++ js/jquery.responsiveTabs.js
@@ -2,7 +2,7 @@
  *  Project: jquery.responsiveTabs.js
  *  Description: A plugin that creates responsive tabs, optimized for all devices
  *  Author: Jelle Kralt (jelle@jellekralt.nl)
- *  Version: 1.3.2
+ *  Version: 1.3.3
  *  License: MIT
  */
 
