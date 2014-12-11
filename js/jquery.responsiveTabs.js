68df6d0c94e5ccc929c088ee3ba2a763e505d6c1
Jelle Kralt <jelle@jellekralt.nl>
2014-12-11 21:57:04 +0100

    Minified JS and 1upped semver

--- js/jquery.responsiveTabs.js
+++ js/jquery.responsiveTabs.js
@@ -2,7 +2,7 @@
  *  Project: jquery.responsiveTabs.js
  *  Description: A plugin that creates responsive tabs, optimized for all devices
  *  Author: Jelle Kralt (jelle@jellekralt.nl)
- *  Version: 1.4.0
+ *  Version: 1.4.1
  *  License: MIT
  */
 
