9572d3d59fb9f04cba120c9a8ee4604db2c4f21f
Jelle Kralt <jelle@jellekralt.nl>
2015-08-14 22:35:45 +0200

    Added Actual tabs to intro

--- site.js
+++ site.js
@@ -91,5 +91,23 @@ $(document).ready(function (){
         }
 	});
 
+    var $tabs = $('#horizontalTab');
+    $tabs.responsiveTabs({
+        rotate: false,
+        startCollapsed: 'accordion',
+        collapsible: 'accordion',
+        setHash: true,
+        disabled: [3,4],
+        activate: function(e, tab) {
+            $('.info').html('Tab <strong>' + tab.id + '</strong> activated!');
+        },
+        activateState: function(e, state) {
+            //console.log(state);
+            $('.info').html('Switched from <strong>' + state.oldState + '</strong> state to <strong>' + state.newState + '</strong> state!');
+        }
+    });
+
+
+
 });
 
