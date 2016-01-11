ef8ebbfd822a12ed573e78d004ad9b86f5fd40ed
Rich Hauck <rhauck@HII-Macbook-Pro.local>
2016-01-11 14:08:39 -0500

    Fixed bug to allow nested uls in tabs

--- js/jquery.responsiveTabs.js
+++ js/jquery.responsiveTabs.js
@@ -137,7 +137,7 @@
      */
     ResponsiveTabs.prototype._loadElements = function() {
         var _this = this;
-        var $ul = this.$element.children('ul');
+        var $ul = this.$element.children('ul:first');
         var tabs = [];
         var id = 0;
 
@@ -278,7 +278,7 @@
      * @param {Event} e - The event that triggers the state change
      */
     ResponsiveTabs.prototype._setState = function(e) {
-        var $ul = $('ul', this.$element);
+        var $ul = $('ul:first', this.$element);
         var oldState = this.state;
         var startCollapsedIsState = (typeof this.options.startCollapsed === 'string');
         var startTab;
