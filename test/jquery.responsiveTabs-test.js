f0df66475eebca4ff3c3d60341e4bc6d11d33b94
Jelle Kralt <jelle@jellekralt.nl>
2016-08-14 18:27:23 +0200

    Added extra tests and upgraded to QUnit 2

--- test/jquery.responsiveTabs-test.js
+++ test/jquery.responsiveTabs-test.js
@@ -1,13 +1,18 @@
 /*global QUnit:false, module:false, test:false, asyncTest:false, expect:false*/
 /*global start:false, stop:false ok:false, equal:false, notEqual:false, deepEqual:false*/
 /*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
-(function($) {
+(function($, QUnit) {
 
-	test('creatable', function() {
-		var $tabs = $('#horizontalTab');
+    QUnit.module('responsive-tabs');
 
-		ok($tabs.responsiveTabs(), 'can be added');
-		equal($tabs.hasClass('r-tabs'), true, 'class was added correctly');
-	});
+    QUnit.test('creatable', function(assert) {
+        var $tabs = $('#horizontalTab');
 
-}(jQuery));
+        assert.ok($tabs.responsiveTabs(), 'can be added');
+        assert.equal($tabs.hasClass('r-tabs'), true, 'container class was added correctly');
+        assert.equal($tabs.find('> ul').hasClass('r-tabs-nav'), true, 'nav class was added correctly');
+        assert.equal($tabs.find('> ul > li').hasClass('r-tabs-tab'), true, 'nav class was added correctly');
+        assert.equal($tabs.find('#tab-1').hasClass('r-tabs-panel'), true, 'panel class was added correctly');
+    });
+
+}(jQuery, QUnit));
